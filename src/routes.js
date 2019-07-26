7e98b2741dbe5cc311c327fd7c08071b050de9ab
Ali_Ahmadiavin <ahmadiavin@gmail.com>
2019-07-26 10:12:51 -0500

    initial commit - linked repo

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import Dashboard from '';
+import Wizard from '';
+
+export default (
+	<Switch>
+		<Route exact path='/' component={Dashboard} />
+		<Route path='/wizard' component={Wizard} />
+	</Switch>
+)
